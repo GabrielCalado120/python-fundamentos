ac92d7a0e2fa65e4d19c87df3e6ebd480d3b690a
GabrielCalado120 <113537735+GabrielCalado120@users.noreply.github.com>
2026-02-12 10:04:15 -0400

    Update classificador_preco.py

--- classificador_preco.py
+++ classificador_preco.py
@@ -1,1 +1,15 @@
+precos = [1.55, 1.42, 1.33, 1.80]
+suporte = 1.40
+resistencia = 1.70
+
+for preco in precos:
+
+    if preco < suporte:
+        print(preco, "- Abaixo do suporte")
+
+    elif preco > resistencia:
+        print(preco, "- Rompimento de resistência")
+
+    else:
+        print(preco, "- Zona neutra")
 
